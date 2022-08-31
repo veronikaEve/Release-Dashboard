5ffeb3e79a673e08dc16a104e7969cffe5033c56
veronika <hunyakvera@gmail.com>
2022-09-01 00:08:18 +0100

    fix custom bootstrap overrides

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
 import App from "./components/app/App";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
