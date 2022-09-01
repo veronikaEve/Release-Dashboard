0702b15cbfd899eb29eb1c27b886ea4be34f9bb3
veronika <hunyakvera@gmail.com>
2022-09-01 20:49:26 +0100

    fix failing secondary-header tests

--- client/src/components/secondary-header/secondary-header.spec.tsx
+++ client/src/components/secondary-header/secondary-header.spec.tsx
@@ -1,28 +1,30 @@
 import { render, screen } from "@testing-library/react";
+import { renderWithRouter } from "../../utils/test-utils/test-utils";
+
 import SecondaryHeader from "./secondary-header";
 
 describe("SecondaryHeader", () => {
   test("should match snapshot", () => {
-    const { asFragment } = render(<SecondaryHeader />);
+    const { asFragment } = renderWithRouter(<SecondaryHeader />);
     expect(asFragment()).toMatchSnapshot();
   });
   test("should display the app version", () => {
-    render(<SecondaryHeader />);
+    renderWithRouter(<SecondaryHeader />);
     const appVersion = screen.getByTestId("app-version-number");
     expect(appVersion).toBeInTheDocument();
   });
   test("should have the view info button", () => {
-    render(<SecondaryHeader />);
+    renderWithRouter(<SecondaryHeader />);
     const viewInfoButton = screen.getByTestId("view-info");
     expect(viewInfoButton).toBeInTheDocument();
   });
   test("should have the roll back button", () => {
-    render(<SecondaryHeader />);
+    renderWithRouter(<SecondaryHeader />);
     const rollBackButton = screen.getByTestId("roll-back");
     expect(rollBackButton).toBeInTheDocument();
   });
   test("should have the new release button", () => {
-    render(<SecondaryHeader />);
+    renderWithRouter(<SecondaryHeader />);
     const newReleaseButton = screen.getByTestId("new-release");
     expect(newReleaseButton).toBeInTheDocument();
   });
